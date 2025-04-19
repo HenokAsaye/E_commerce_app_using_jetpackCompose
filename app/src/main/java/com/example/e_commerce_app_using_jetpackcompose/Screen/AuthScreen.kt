e3693e60ae804d4e683e66e4db5b87d853f9d4bc
Henok Asaye <henokasaye77@gmail.com>
2025-04-19 15:45:04 +0300

    modify the buttons to navigate to their respective screens

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Screen/AuthScreen.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Screen/AuthScreen.kt
@@ -1,6 +1,7 @@
 package com.example.e_commerce_app_using_jetpackcompose.Screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,12 +9,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
@@ -22,10 +25,11 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import com.example.e_commerce_app_using_jetpackcompose.R
 
 @Composable
-fun AuthScreen(modifier: Modifier = Modifier){
+fun AuthScreen(modifier: Modifier = Modifier, navController: NavHostController){
     Column(
         modifier = modifier.fillMaxSize()
             .padding(32.dp),
@@ -34,8 +38,12 @@ fun AuthScreen(modifier: Modifier = Modifier){
     ){
         Image(painter = painterResource(id = R.drawable.banner),
             contentDescription ="banner",
-            modifier = modifier.fillMaxWidth()
+            modifier = modifier
+                .fillMaxWidth()
                 .height(300.dp)
+                .clip(RoundedCornerShape(16.dp)) // border radius
+                .border(2.dp, Color.Gray, RoundedCornerShape(16.dp)) // optional border
+
         )
         Spacer(modifier = Modifier.height(20.dp))
 
@@ -50,7 +58,6 @@ fun AuthScreen(modifier: Modifier = Modifier){
             )
         )
         Spacer(modifier = Modifier.height(20.dp))
-
         Text(
             text="Best ecom app with Best price",
             style = TextStyle(
@@ -59,9 +66,10 @@ fun AuthScreen(modifier: Modifier = Modifier){
 
             )
         )
-
         Spacer(modifier = Modifier.height(20.dp))
-        Button(onClick = { },
+        Button(onClick = {
+            navController.navigate("login")
+        },
             modifier = Modifier.fillMaxWidth()
                 .height(60.dp)
             ) {
@@ -73,20 +81,19 @@ fun AuthScreen(modifier: Modifier = Modifier){
                 )
             )
         }
-
         Spacer(modifier = Modifier.height(20.dp))
-        OutlinedButton(onClick = { },
+        OutlinedButton(onClick = {
+            navController.navigate("signup")
+        },
             modifier = Modifier.fillMaxWidth()
                 .height(60.dp)
         ) {
             Text(
                 text = "Sign Up",
                 style = TextStyle(
-                    color = Color.White,
                     fontSize = 22.sp
                 )
             )
         }
-
     }
 }
