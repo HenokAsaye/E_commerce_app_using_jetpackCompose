bc9802b92e659ee2b73bb7c190f7f0e0ce48e2af
Henok Asaye <henokasaye77@gmail.com>
2025-04-19 12:27:07 +0300

    initial commit

---
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.e_commerce_app_using_jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.e_commerce_app_using_jetpackcompose.ui.theme.Ecommerce_app_using_jetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Ecommerce_app_using_jetpackComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
---
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Screen/AuthScreen.kt
@@ -0,0 +1,9 @@
+package com.example.e_commerce_app_using_jetpackcompose.Screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun AuthScreen(modifier: Modifier = Modifier){
+
+}
