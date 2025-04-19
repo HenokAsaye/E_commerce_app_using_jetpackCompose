689d9b02d3761059b62b83532ba704a97e7f1079
Henok Asaye <henokasaye77@gmail.com>
2025-04-19 15:45:42 +0300

    create the UI of the Login screen

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Screen/LoginScreen.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Screen/LoginScreen.kt
@@ -1,2 +1,24 @@
 package com.example.e_commerce_app_using_jetpackcompose.Screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+
+fun LoginScreen(modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+    }
+}
