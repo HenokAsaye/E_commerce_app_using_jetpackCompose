097a60b8ce32982fe9a7dd0b35016e0a69bd376a
Henok Asaye <henokasaye77@gmail.com>
2025-04-19 15:46:14 +0300

    create the Ui of the sign UP Screen

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Screen/SignUpScreen.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Screen/SignUpScreen.kt
@@ -1,2 +1,110 @@
 package com.example.e_commerce_app_using_jetpackcompose.Screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.e_commerce_app_using_jetpackcompose.R
+
+@Composable
+fun SignUpScreen(modifier: Modifier = Modifier) {
+    var email by remember { mutableStateOf("") }
+    var name by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState) // 👈 This makes it scrollable
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Hello There!",
+            modifier = Modifier.fillMaxWidth(),
+            style = TextStyle(
+                fontSize = 30.sp,
+                fontFamily = FontFamily.Monospace,
+                fontWeight = FontWeight.Bold,
+            )
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        Text(
+            text = "Create an Account!",
+            modifier = Modifier.fillMaxWidth(),
+            style = TextStyle(fontSize = 22.sp)
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.img),
+            contentDescription = "Sign up image",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        )
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email Address") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Your Name") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier.fillMaxWidth(),
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+
+        Button(
+            onClick = {},
+            modifier = Modifier.fillMaxWidth()
+                .height(60.dp)
+        ){
+            Text(
+                text = "Sign Up",
+                style = TextStyle(
+                    fontSize = 22.sp,
+                )
+            )
+        }
+    }
+}
