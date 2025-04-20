c2068e55901c5bc075de36451a804bbb87e1aea2
Henok Asaye <henokasaye77@gmail.com>
2025-04-20 23:55:08 +0300

    created for the banner section in the HOMe Screen

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/components/Banner.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/components/Banner.kt
@@ -1,2 +1,32 @@
 package com.example.e_commerce_app_using_jetpackcompose.components
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.SemanticsProperties.Text
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+
+@Composable
+
+fun Banner(modifier:Modifier = Modifier){
+
+    var bannerList by remember{
+        mutableStateOf<List<String>>(emptyList())
+    }
+    LaunchedEffect(Unit) {
+        Firebase.firestore.collection("data")
+            .document("Banner")
+            .get().addOnCompleteListener(){
+                bannerList = it.result.get("URL") as List<String>
+            }
+    }
+    Text(
+        text = (bannerList.toString())
+    )
+}
