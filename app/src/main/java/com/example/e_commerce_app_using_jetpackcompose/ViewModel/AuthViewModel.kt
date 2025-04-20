b0c1f08b934f2d1d323db5bfa3f1ff6af65abfce
Henok Asaye <henokasaye77@gmail.com>
2025-04-20 19:42:45 +0300

    add the sign up functionality

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/ViewModel/AuthViewModel.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/ViewModel/AuthViewModel.kt
@@ -1,12 +1,38 @@
 package com.example.e_commerce_app_using_jetpackcompose.ViewModel
 
 import androidx.lifecycle.ViewModel
+import com.example.e_commerce_app_using_jetpackcompose.model.userModel
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
+import com.google.firebase.firestore.firestore
 
 class AuthViewModel: ViewModel(){
 
+    private val auth = Firebase.auth
+    private val firestore = Firebase.firestore
 
 
-    fun signup(){}
+    fun signup(email:String,name:String,password:String,onResult:(Boolean,String?) ->Unit ){
+        auth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener{
+                if(it.isSuccessful){
+                    var userId = it.result?.user?.uid
+                    val userModel = userModel(email,name,userId!!)
+                    firestore.collection("users").document(userId)
+                        .set(userModel)
+                        .addOnCompleteListener{ dbTask ->
+                            if(dbTask.isSuccessful){
+                                onResult(true,null)
+                            }else{
+                                onResult(false,"Some Thing Went Wrong")
+                            }
+                        }
+
+                }else{
+                    onResult(false,it.exception?.localizedMessage)
+                }
+            }
+    }
 
 
     fun login(){}
