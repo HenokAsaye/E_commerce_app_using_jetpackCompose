239a11699ecb5ba6c287340d21d31d5485225ab0
Henok Asaye <henokasaye77@gmail.com>
2025-04-20 20:33:12 +0300

    create Home Screen

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Screen/HomeScreen.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Screen/HomeScreen.kt
@@ -1,2 +1,31 @@
 package com.example.e_commerce_app_using_jetpackcompose.Screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+
+fun HomeScreen(modifier:Modifier = Modifier){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Home Screen"
+        )
+
+    }
+}
+
+
