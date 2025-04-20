b08ba2caa2c25e4348aedf5f82d51fe64241be7b
Henok Asaye <henokasaye77@gmail.com>
2025-04-20 23:53:46 +0300

    creatd for the personal profile page

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Pages/HomePage.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Pages/HomePage.kt
@@ -1,2 +1,22 @@
 package com.example.e_commerce_app_using_jetpackcompose.Pages
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.e_commerce_app_using_jetpackcompose.components.Banner
+import com.example.e_commerce_app_using_jetpackcompose.components.Header
+
+@Composable
+
+
+fun Homepage(modifier: Modifier=Modifier){
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Header(modifier)
+        Banner(modifier)
+    }
+}
--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Pages/ProfilePage.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Pages/ProfilePage.kt
@@ -1,2 +1,12 @@
 package com.example.e_commerce_app_using_jetpackcompose.Pages
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+
+
+fun Profilepage(modifier: Modifier=Modifier){
+    Text(text = "Profile Page")
+}
