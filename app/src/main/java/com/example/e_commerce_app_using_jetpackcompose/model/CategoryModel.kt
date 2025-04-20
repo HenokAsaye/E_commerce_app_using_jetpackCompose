132eddb0e5d410075d2c601ce64e92d173b335d6
Henok Asaye <henokasaye77@gmail.com>
2025-04-21 02:56:26 +0300

    data class to handle the category data

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/model/CategoryModel.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/model/CategoryModel.kt
@@ -1,3 +1,7 @@
 package com.example.e_commerce_app_using_jetpackcompose.model
 
-data class CategoryModel()
+data class CategoryModel(
+    val id: String = "",
+    val name : String = "",
+    val imageurl : String =  ""
+)
