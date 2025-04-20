fa2ec9b7bde85ce51bf3edfcfbe350cb47edbf2e
Henok Asaye <henokasaye77@gmail.com>
2025-04-21 00:52:59 +0300

    done with the banner of the Home Screen

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/components/Banner.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/components/Banner.kt
@@ -1,5 +1,15 @@
 package com.example.e_commerce_app_using_jetpackcompose.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -8,9 +18,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.google.firebase.Firebase
 import com.google.firebase.firestore.firestore
+import com.tbuonomo.viewpagerdotsindicator.compose.DotsIndicator
+import com.tbuonomo.viewpagerdotsindicator.compose.model.DotGraphic
+import com.tbuonomo.viewpagerdotsindicator.compose.type.ShiftIndicatorType
 
 @Composable
 
@@ -26,7 +43,38 @@ fun Banner(modifier:Modifier = Modifier){
                 bannerList = it.result.get("URL") as List<String>
             }
     }
-    Text(
-        text = (bannerList.toString())
-    )
+    Column(
+        modifier = modifier
+    ){
+        val pager = rememberPagerState(0) {
+            bannerList.size
+        }
+        HorizontalPager(
+            state = pager,
+            pageSpacing = 24.dp
+        ) {
+            AsyncImage(
+                model = bannerList.get(it),
+                contentDescription = "Banner Image",
+                modifier = Modifier.fillMaxWidth()
+                    .clip(RoundedCornerShape(20.dp))
+                    .height(180.dp)
+                    .background(color = Color.Blue)
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        DotsIndicator(
+            dotCount = bannerList.size,
+            type = ShiftIndicatorType(
+                DotGraphic(
+                    color = MaterialTheme.colorScheme.primary,
+                    size = 6.dp
+            )
+            ),
+            pagerState = pager
+        )
+
+    }
 }
+
+
