1d71f43d13ebb28341c27099cd9f00ebaf5b9c5e
Henok Asaye <henokasaye77@gmail.com>
2025-04-21 02:56:59 +0300

    modify the navigation

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/ui/theme/AppNavigation.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/ui/theme/AppNavigation.kt
@@ -2,9 +2,11 @@ package com.example.e_commerce_app_using_jetpackcompose.ui.theme
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.e_commerce_app_using_jetpackcompose.Pages.CategoryProduct
 import com.example.e_commerce_app_using_jetpackcompose.Screen.AuthScreen
 import com.example.e_commerce_app_using_jetpackcompose.Screen.HomeScreen
 import com.example.e_commerce_app_using_jetpackcompose.Screen.LoginScreen
@@ -16,6 +18,7 @@ import com.google.firebase.auth.auth
 
 fun AppNavigation(modifier : Modifier = Modifier){
     val navController = rememberNavController()
+    GlobalNavigation.navController = navController
     val isLoggedin = Firebase.auth.currentUser != null
     val firstPage = if(isLoggedin) "home" else "auth"
 
@@ -35,6 +38,14 @@ fun AppNavigation(modifier : Modifier = Modifier){
         composable("home"){
             HomeScreen(modifier,navController)
         }
+        composable("productDetail/{categoryId}"){
+            CategoryProduct(modifier,navController)
+        }
     }
 
+}
+
+
+object GlobalNavigation{
+    lateinit var navController: NavHostController
 }
