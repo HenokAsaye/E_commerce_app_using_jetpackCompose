63cf8d3b214ecab9fe2edfbd3f0133bc6de78069
Henok Asaye <henokasaye77@gmail.com>
2025-04-20 20:52:23 +0300

    fix bug on the sigh up screen

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Screen/SignUpScreen.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Screen/SignUpScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -17,15 +18,24 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.example.e_commerce_app_using_jetpackcompose.AppUtill.showToast
 import com.example.e_commerce_app_using_jetpackcompose.R
+import com.example.e_commerce_app_using_jetpackcompose.ViewModel.AuthViewModel
 
 @Composable
-fun SignUpScreen(modifier: Modifier = Modifier) {
+fun SignUpScreen(modifier: Modifier = Modifier,navController: NavController, authViewModel:  AuthViewModel= viewModel()) {
     var email by remember { mutableStateOf("") }
     var name by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var isLoading by remember {
+        mutableStateOf(false)
+    }
 
     val scrollState = rememberScrollState()
+    val context = LocalContext.current
 
     Column(
         modifier = Modifier
@@ -95,12 +105,26 @@ fun SignUpScreen(modifier: Modifier = Modifier) {
 
 
         Button(
-            onClick = {},
+            onClick = {
+                isLoading = true
+                authViewModel.signup(email,name,password){success,errorMessage ->
+                    if(success){
+                        isLoading = false
+                        navController.navigate("home"){
+                            popUpTo("auth"){inclusive=true}
+                        }
+                    }else{
+                        isLoading =  false
+                        showToast(context,errorMessage?:"some thing Went Wrong")
+                    }
+                }
+            },
+            enabled = !isLoading,
             modifier = Modifier.fillMaxWidth()
                 .height(60.dp)
         ){
             Text(
-                text = "Sign Up",
+                text = if(isLoading) "Creating acccount" else "Sign Up",
                 style = TextStyle(
                     fontSize = 22.sp,
                 )
