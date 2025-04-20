4fffa8aeb88f47988e7c8aa8aae865dd48d8397b
Henok Asaye <henokasaye77@gmail.com>
2025-04-20 23:52:45 +0300

    created for the favourite page

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Pages/FavouritePage.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Pages/FavouritePage.kt
@@ -1,2 +1,12 @@
 package com.example.e_commerce_app_using_jetpackcompose.Pages
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+
+
+fun FavouritePage(modifier: Modifier=Modifier){
+    Text(text = "Favourite Page")
+}
