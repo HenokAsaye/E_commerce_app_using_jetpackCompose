bd32d2c06e15080e08119a0cf5390dbcc8bd0532
Henok Asaye <henokasaye77@gmail.com>
2025-04-20 22:06:38 +0300

    add bottom scaffold to handle navigation between pages

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Screen/HomeScreen.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Screen/HomeScreen.kt
@@ -5,11 +5,26 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.google.firebase.Firebase
@@ -18,30 +33,52 @@ import com.google.firebase.auth.auth
 @Composable
 
 fun HomeScreen(modifier:Modifier = Modifier,navController: NavController){
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(32.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(
-            text = "Home Screen"
-        )
-        Button(
-            onClick = {
-                Firebase.auth.signOut()
-                navController.navigate("auth"){
-                    popUpTo("home"){inclusive=true}
-                }
-            }
-        ) {
-            Text(
-                text="Log Out"
-            )
-        }
 
+    val NavItemList = listOf(
+        NavItem("Home",Icons.Default.Home),
+        NavItem("Favourite",Icons.Default.Favorite),
+        NavItem("Cart",Icons.Default.ShoppingCart),
+        NavItem("Profile",Icons.Default.Person),
+    )
+    var selectedIndex by remember {
+        mutableStateOf(0)
     }
+   Scaffold(
+       bottomBar = {
+           NavigationBar {
+               NavItemList.forEachIndexed{index,navitem ->
+                   NavigationBarItem(
+                       selected = index == selectedIndex,
+                       onClick = {
+                           selectedIndex = index
+                       },
+                       icon = {
+                           Icon(imageVector = navitem.icon, contentDescription = navitem.label)
+
+                       },
+                       label = {Text(text=navitem.label)}
+                   )
+
+               }
+           }
+       }
+
+   ){
+       ContentScreen(modifier = modifier.padding(it))
+   }
 }
+
+
+
+@Composable
+fun ContentScreen(modifier: Modifier = Modifier){
+
+}
+
+data class NavItem(
+    val label:String,
+    val icon:ImageVector
+)
+
 
 
