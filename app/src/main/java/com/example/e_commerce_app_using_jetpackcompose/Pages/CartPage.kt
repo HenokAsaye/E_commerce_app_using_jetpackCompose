0150a439d1003e9ba1a2fba183133ec8d271b404
Henok Asaye <henokasaye77@gmail.com>
2025-04-20 23:51:47 +0300

    created for the cart page

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Pages/CartPage.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Pages/CartPage.kt
@@ -1,2 +1,12 @@
 package com.example.e_commerce_app_using_jetpackcompose.Pages
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+
+
+fun CartPage(modifier: Modifier=Modifier){
+    Text(text = "Cart Page")
+}
