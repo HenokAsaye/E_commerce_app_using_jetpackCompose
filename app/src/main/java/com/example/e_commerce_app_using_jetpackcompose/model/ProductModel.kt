9710785ed5427d49ec4820793decd553a31c29ad
Henok Asaye <henokasaye77@gmail.com>
2025-04-21 15:48:41 +0300

    data class for the products data

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/model/ProductModel.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/model/ProductModel.kt
@@ -1,3 +1,12 @@
 package com.example.e_commerce_app_using_jetpackcompose.model
 
-data class ProductModel()
+data class ProductModel(
+    val id:String,
+    val title:String,
+    val description:String,
+    val price: Int,
+    val actualprice:Int,
+    val category: String,
+    val images:String,
+
+)
