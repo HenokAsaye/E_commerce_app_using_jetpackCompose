d9fbbef1bebc6ed5af817e1544c77cbad9ecf45f
Henok Asaye <henokasaye77@gmail.com>
2025-04-22 00:21:45 +0300

    products details page

---
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/Pages/productPage.kt
@@ -0,0 +1,11 @@
+package com.example.e_commerce_app_using_jetpackcompose.Pages
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+
+
+fun ProductDetailPage(modifier:Modifier =Modifier,productId:String){
+
+}
