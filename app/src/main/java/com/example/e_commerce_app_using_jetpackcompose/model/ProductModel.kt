366bd0c9c1da10765fddeee5b5e0a3260e1cd3ab
Henok Asaye <henokasaye77@gmail.com>
2025-04-22 10:03:59 +0300

    fix bugs on the data class

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/model/ProductModel.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/model/ProductModel.kt
@@ -1,12 +1,12 @@
 package com.example.e_commerce_app_using_jetpackcompose.model
 
 data class ProductModel(
-    val id:String,
-    val title:String,
-    val description:String,
-    val price: String,
-    val actualprice:String,
-    val category: String,
-    val images:String,
+    val id:String = "",
+    val title:String = "",
+    val description:String ="",
+    val price: String = "",
+    val actualprice:String = "",
+    val category: String = "",
+    val images:Array<String> = emptyArray(),
 
-)
+    )
