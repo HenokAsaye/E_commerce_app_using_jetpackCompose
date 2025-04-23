53f6e053a0a113f153270567e3305fddcf4ed6a7
Henok Asaye <henokasaye77@gmail.com>
2025-04-23 23:12:43 +0300

    create composable for the cart page

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/components/cartiem.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/components/cartiem.kt
@@ -1,2 +1,10 @@
 package com.example.e_commerce_app_using_jetpackcompose.components
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+
+fun cartItemView(modfier: Modifier = Modifier, productId:String, qty:Long){
+
+}
