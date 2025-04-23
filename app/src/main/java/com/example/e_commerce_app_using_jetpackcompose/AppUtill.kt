514e72baa9767fe563f960a89300c6338de16d7d
Henok Asaye <henokasaye77@gmail.com>
2025-04-23 22:47:49 +0300

    add to the cart function to add items to the cart

--- app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/AppUtill.kt
+++ app/src/main/java/com/example/e_commerce_app_using_jetpackcompose/AppUtill.kt
@@ -2,10 +2,35 @@ package com.example.e_commerce_app_using_jetpackcompose
 
 import android.content.Context
 import android.widget.Toast
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
 
 object AppUtill {
     fun showToast(context: Context, message:String){
         Toast.makeText(context,message,Toast.LENGTH_LONG).show()
+    }
+
+    fun addItemToCart(productId:String, context:Context){
+        val userDoc = Firebase.firestore.collection("users")
+            .document(FirebaseAuth.getInstance().currentUser?.uid!!)
+        userDoc.get().addOnCompleteListener{
+            if(it.isSuccessful){
+                val currentCart = it.result.get("cartItems") as? Map<String,Long> ?: emptyMap()
+                val currentQuanity = currentCart[productId] ?:0
+                val updateQuanity = currentQuanity + 1
+
+                var updatedCart = mapOf("cartItems.$productId" to updateQuanity)
+                userDoc.update(updatedCart)
+                    .addOnCompleteListener{
+                        if(it.isSuccessful){
+                            showToast(context,"Item added to the cart")
+                        }else{
+                            showToast(context,"Failed adding item to the cart")
+                        }
+                    }
+            }
+        }
     }
 
 }
